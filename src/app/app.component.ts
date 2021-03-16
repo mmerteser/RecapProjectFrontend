796b015af15bce57efea479017d184c95409c70e
Mert ESER <merteser@outlook.com>
2021-03-17 00:12:23 +0300

    Camp frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'northwind';
+  title: string = 'northwind';
 }
+
+// component html'in datasının yönetildiği yerdir
+// [Array]
+// class'a attribute olarak @Component verilmişse component class demektir
